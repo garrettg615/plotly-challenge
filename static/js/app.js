1492a950113e8f80dac756c702d04cdab456cb94
Garrett Gomez <garrettgomez@Garretts-MBP-2.fios-router.home>
2021-05-11 23:35:55 -0400

    additional styling added to index.hrml, app.js completed graph1, working on bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,136 @@
 console.log('this is the beginning!')
 
-
+// define function to display default graph for ID selector
 function init() {
-    d3.json("static/data/samples.json").then((abc) => {
+    d3.json("static/data/samples.json").then((incomingData) => {
+        var sampVal = incomingData.samples[0].sample_values;
+        var otuIds = incomingData.samples[0].otu_ids;
+
+        var top10samp = sampVal.slice(0,10);
+        var top10otu = otuIds.slice(0,10);
+
         data = [{
-            x: abc.samples[0].sample_values,
-            y: abc.samples[0].otu_ids,
             type:'bar',
+            x: top10samp,
+            y: top10otu,
             orientation:'h'
         }];
 
-        Plotly.newPlot("graph1", data)
+        layout = {
+            title: "Top 10 Bacteria in Naval",
+            xaxis: {
+                dtick: 50
+            },
+            yaxis: {
+                autorange: 'reversed',
+                type: 'category'
+            },
+        };
+
+        Plotly.newPlot("graph1", data, layout);
         
-    })
+    });
 }
 
-init()
+// assign test subject IDs in drop down
+d3.json("static/data/samples.json").then((incomingData) => {
+    var subjectIds = incomingData.names;
 
+    subjectIds.forEach((id) => {
+        var dropDown = d3.select("#dropdown-menu");
+        var option = dropDown.append("option");
+        option.text(id);
+    });
+    // console.log(subjectIds);
+});
 
-// var ids = [];
-// var sampleValues = [];
-// var otuIds = [];
-// var otuLabels = [];
+// create event listener for dropdown
 
+d3.select("#dropdown-menu").on('change', updatePlotly);
 
-// d3.json("static/data/samples.json").then((data) => {
-//     var research = data.samples;
-//     // console.log(research)
+function updatePlotly() {
+    var dropValue = d3.select("#dropdown-menu").property('value')
+    console.log(dropValue);
 
-//     research.forEach((subject) => {
-//         ids.push(subject.id)
-//         sampleValues.push(subject.sample_values)
-//         otuIds.push(subject.otu_ids)
-//         otuLabels.push(subject.otu_labels)
-//     })
+    d3.json("static/data/samples.json").then((incomingData) => {
+        var samp_df = incomingData.samples;
+        
+        x = [];
+        y = [];
+
+        samp_df.forEach((row) => {
+            if (dropValue === row.id) {
+                var top10samps = row.sample_values.slice(0, 10);
+                var top10otus = row.otu_ids.slice(0, 10);
     
+                x = top10samps;
+                y = top10otus;
 
-// });
+            }
+        });
+
+        data = [{
+            type:'bar',
+            x: x,
+            y: y,
+            orientation:'h'
+        }];
+
+        layout = {
+            title: "Top 10 Bacteria in Naval",
+            xaxis: {
+                dtick: 50
+            },
+            yaxis: {
+                autorange: 'reversed',
+                type: 'category'
+            },
+        };
+        
+        Plotly.newPlot("graph1", data, layout);
+
+    });
+};
+
+function buildGraph() {
+    d3.json("static/data/samples.json").then((incomingData) => {
+        var samples = incomingData.samples;
+
+        var otuIds = [];
+        var sampValues = [];
+        var otuLabels = [];
+
+        samples.forEach((row) => {
+            otuIds.push(row.otu_ids)
+            sampValues.push(row.sample_values)
+            otuLabels.push(row.otu_labels)
+        });
+        
+        // console.log(otuIds);
+        // console.log(sampValues);
+        // console.log(otuLabels);
+
+        var data = [{
+            x: otuIds,
+            y: sampValues,
+            mode: 'markers',
+            marker: {
+                size: otuLabels
+            }
+        }];
+
+        var layout = {
+            title: "Bubble chart"
+        };
+
+        Plotly.newPlot("graph2", data, layout)
+    });
+};
+
+
+// buildGraph();
+init();
 
 
 
-// // console.log(ids)
-// // console.log(sampleValues)
-// // console.log(otuIds)
-// // console.log(otuLabels)
 
